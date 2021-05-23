0ca55142f8cc05dc485a5fbc10b47da71fd2c947
Bekzhan Satarov <satarovbekzhan@gmail.com>
2021-05-23 04:07:14 +0200

    Portfolio design upgrade

--- me/js/app.js
+++ me/js/app.js
@@ -1,8 +1,8 @@
 new fullScroll({
     mainElement: "main",
-    displayDots: true,
+    displayDots: false,
     dotsPosition: "right",
-    animateTime: 0.7,
+    animateTime: 0.5,
     animateFunction: "ease",
 });
 
@@ -61,3 +61,15 @@ function info(txt) {
     section.innerHTML = '';
     section.appendChild(p);
 }
+
+// disable code inspection
+document.onkeydown = function (e) {
+    if (event.keyCode === 123) {
+        return false
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode === "I".charCodeAt(0)) return false
+    if (e.ctrlKey && e.shiftKey && e.keyCode === "C".charCodeAt(0)) return false
+    if (e.ctrlKey && e.shiftKey && e.keyCode === "J".charCodeAt(0)) return false
+    if (e.ctrlKey && e.keyCode === "U".charCodeAt(0)) return false
+}
+document.addEventListener('contextmenu', e => e.preventDefault())
